8295ed513eb06096fd510249343fc2173ecf43a3
TheTimo87 <timo.hellmund@gmx.de>
2018-11-19 19:34:33 +0100

    Added Game Model + Controller

---
+++ MyGamesList/MyGamesList/Controllers/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyGamesList.Controllers
+{
+    public class GameController : Controller
+    {
+        // GET: Game
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Detail(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+    }
+}
---
+++ MyGamesList/MyGamesList/Models/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyGamesList.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public bool IsCompleted { get; set; }
+
+
+    }
+}
